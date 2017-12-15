a8240b08969e58abb5444329577bb2356db36cf1
Frederico <frediramos@gmail.com>
2017-12-15 02:28:44 +0000

    fill f_reposicoes

--- fill_mm.sql
+++ fill_mm.sql
@@ -21,3 +21,22 @@ BEGIN
 END;
 
 $$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION load_reposicoes()
+RETURNS VOID AS $$
+DECLARE prod CURSOR FOR SELECT * FROM produto NATURAL JOIN planograma;
+BEGIN
+	FOR prod_reg IN prod LOOP
+		INSERT INTO f_reposicoes(cean,timeid,unidades) VALUES 
+			((SELECT cean FROM d_produto WHERE d_produto.cean = prod_reg.ean),
+			 (SELECT timeid FROM d_tempo WHERE 	d_tempo.dia = date_part('day',prod_reg.data)
+			 	 AND d_tempo.mes = date_part('month',prod_reg.data) 
+			 	 AND d_tempo.ano = date_part('year',prod_reg.data)),
+			 (SELECT prod_reg.unidades));
+	
+	END LOOP;
+		 
+END;
+
+$$ LANGUAGE plpgsql;
